f3583c2eacf09340e7d70f4fff8e61c487dc23e9
ShayanMS <Shayan.Keshmiri@gmx.de>
2017-07-11 17:11:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 function onClick(){
+	alert("Hello! I am an alert box!!");
 		let submitBtn = document.getElementById("submit");
 		let input = document.getElementById("text");
     
     let message = input.value;
-		alert("Hello! I am an alert box!!");
+		
 
 }
