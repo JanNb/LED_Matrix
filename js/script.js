debc7621b00375f9afe92b4f3bafe38a7a873ebf
ShayanMS <Shayan.Keshmiri@gmx.de>
2017-07-11 16:21:15 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,8 @@
-function submit(){
-	// define event listener
-	function onClick(){
+function onClick(){
 		let submitBtn = document.getElementById("submit");
 		let input = document.getElementById("text");
     
     let message = input.value;
 		alert("Hello! I am an alert box!!");
-		
-	}
-	
-	// set event listener
-	let redSlider = document.getElementById("slider-red");
-	redSlider.addEventListener("input", onSliderChanged);
-	let greenSlider = document.getElementById("slider-green");
-	greenSlider.addEventListener("input", onSliderChanged);
-	let blueSlider = document.getElementById("slider-blue");
-	blueSlider.addEventListener("input", onSliderChanged);
-	// initialize the color
-	onSliderChanged();
 
 }
